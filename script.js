1bbb09491093378f3e6b1a8a00ca4647aa80c247
tayyaba <hilanapoloa236@gmail.com>
2025-11-18 08:19:14 +0500

    paper game

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-let btn = document.querySelector('#btn')
-let box = document.querySelector('.box')
-
+let btn = document.querySelector('button')
+let main=document.querySelector('main')
 
 btn.addEventListener('click',function(){
-    let c1=Math.floor(Math.random()*256)
-    let c2=Math.floor(Math.random()*256)
-    let c3=Math.floor(Math.random()*256)
+    let div=document.createElement('div')
 
-    box.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+    let x=Math.random()*100
+    let y=Math.random()*100
+
+        let c1=Math.floor(Math.random()*256)
+        let c2=Math.floor(Math.random()*256)
+        let c3=Math.floor(Math.random()*256)
+
+     let r=Math.floor(Math.random()*361)  
+
+
+
+    div.style.backgroundColor= `rgb(${c1},${c2},${c3})`
+    div.style.height='100px'
+    div.style.width='100px'
+    div.style.position='absolute'
+    div.style.top=x+'%'
+    
+    div.style.left=y+'%'
+    div.style.rotate=x+'deg'
+
+    main.appendChild(div)
+
+
 })
