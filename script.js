e4da78bd5f247f0cc6661d4fa77eb9454c900c60
tayyaba <hilanapoloa236@gmail.com>
2025-11-19 14:12:48 +0500

    dowloader

--- script.js
+++ script.js
@@ -1,71 +1,30 @@
-let btn = document.querySelector('button')
-let main=document.querySelector('main')
+let btn=document.querySelector('button')
+let p=document.querySelector('p')
+let fil=document.querySelector('.fil')
+let per=0
+let w=0
 
-const loveQuotes = [
-  "You are my sunshine.",
-  "I love you to the moon and back.",
-  "You complete me.",
-  "My heart beats for you.",
-  "You are my everything.",
-  "Love you more each day.",
-  "You make my heart smile.",
-  "Forever and always.",
-  "You are my dream come true.",
-  "My love for you is endless.",
-  "You are my happy place.",
-  "Soulmates forever.",
-  "You have my whole heart.",
-  "Better together.",
-  "You are my always.",
-  "Love at first sight.",
-  "You're the one for me.",
-  "My heart belongs to you.",
-  "You make life beautiful.",
-  "Perfect together."
-];
+let work=function(){
+  let int=setInterval(function(){
+  per++
+  w++
+  //  console.log(w)
+      // console.log(p)
 
-btn.addEventListener('click',function(){
+  
+  fil.style.width=w+'%'
+  p.innerHTML=per+'%'
+ },20)
 
-    let h2=document.createElement('h2')
-    
-    //position 
-    let x=Math.random()*100
-    let y=Math.random()*100
-       
+setTimeout(() => {
+   clearInterval(int)
+}, 2000);
+}
 
-    //color
-        let c1=Math.floor(Math.random()*256)
-        let c2=Math.floor(Math.random()*256)
-        let c3=Math.floor(Math.random()*256)
-      
-     //rotation
-      let r=Math.floor(Math.random()*361)  
+console.log(work)
 
-      //index
-      let i=Math.floor(Math.random()*loveQuotes.length) 
+btn.addEventListener('click',work)
+
+btn.removeEventListener('click',work)
 
 
-      //giving text
-       h2.innerHTML=loveQuotes[i]
-
-       //for scale
-      let s=Math.floor(Math.random()*3)
-
-
-
-
-    //style
-    h2.style.color= `rgb(${c1},${c2},${c3})`
-    h2.style.position='absolute'
-    h2.style.top=x+'%'
-    
-    h2.style.left=y+'%'
-    h2.style.rotate=x+'deg'
-    h2.style.overflow="hidden"
-    h2.style.scale=s
-     
-    //appened to main
-    main.appendChild(h2)
-
-
-})
