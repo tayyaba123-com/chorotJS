e4fd3c5fe1917a03648678e008487680c5d03236
tayyaba <hilanapoloa236@gmail.com>
2025-11-22 14:41:54 +0500

    magial cursor

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ body.addEventListener('mousemove',function(elem){
    console.log(elem)
    img.style.top=elem.y+'px'
     img.style.left=elem.x+'px'
-    img.style.transform='translate(50% 50%)'
+  
 
 })
