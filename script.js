09cc42fb31c69c41744ef5832a8c9d16bf1a8d7e
tayyaba <hilanapoloa236@gmail.com>
2025-11-22 13:45:24 +0500

    dowload-cotanier

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
 let btn=document.querySelector('button')
-let p=document.querySelector('p')
-let fil=document.querySelector('.fil')
+let p=document.querySelector('h2')
+let fil=document.querySelector('.progress-fill')
 let per=0
 let w=0
 
-let work=function(){
-  let int=setInterval(function(){
-  per++
-  w++
-  //  console.log(w)
-      // console.log(p)
+//    btn.addEventListener('click',function(){
+//   let int=setInterval(function(){
+//   per++
+//   w++
+ 
 
   
+//   fil.style.width=w+'%'
+//   p.innerHTML=per+'%'
+//  },20)
+
+// setTimeout(() => {
+//    clearInterval(int)
+// }, 2000)
+
+
+// },{once:true})
+
+
+//    let handleCick=function(){
+//   let int=setInterval(function(){
+//   per++
+//   w++
+ 
+
+  
+//   fil.style.width=w+'%'
+//   p.innerHTML=per+'%'
+//  },20)
+
+// setTimeout(() => {
+//    clearInterval(int)
+// }, 2000)
+
+
+// btn.removeEventListener('click',handleCick)
+
+// }
+
+// btn.addEventListener('click',handleCick)
+
+
+   btn.addEventListener('click',function(){
+      let num=50+Math.floor(Math.random()*50)
+    btn.style.pointerEvents='none'
+    console.log(`dowloing will complete in ${num/10} seconds`)
+  let int=setInterval(function(){
+  per++
+  w++  
   fil.style.width=w+'%'
   p.innerHTML=per+'%'
- },20)
+ },num)
 
+ 
 setTimeout(() => {
    clearInterval(int)
-}, 2000);
-}
+   btn.innerHTML="Dowloaded"
+   btn.style.opacity='0.5'
+}, num*100)
+  
+},)
 
-console.log(work)
 
-btn.addEventListener('click',work)
-
-btn.removeEventListener('click',work)
 
 
