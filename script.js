b7ebafb3909bd59972fa62426b9c418376a808ed
tayyaba <hilanapoloa236@gmail.com>
2025-11-26 16:13:52 +0500

    keyboard piano

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-let img=document.querySelector('img')
-let body=document.body
+let main=document.querySelector('main')
+ let boxes=document.querySelectorAll(' .box')
+ 
 
-body.addEventListener('mousemove',function(elem){
-   console.log(elem)
-   img.style.top=elem.y+'px'
-    img.style.left=elem.x+'px'
-  
+ const capitalAlphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+ 
+ capitalAlphabets.forEach(function(value,i){
+    boxes[i].innerText=value
+    
+    
+    })
 
+ 
+
+boxes.forEach(function(val){
+    val.addEventListener('click',function(){
+     sounds(val.innerHTML)
+    })
 })
+
+
+
+   function sounds(key){
+         audio=new Audio(`audio/${key}.mp3`)
+         audio.play()
+        
+   }
+
+ 
