3978920ebacb3003a837fe00ce82bdce16712396
tayyaba <hilanapoloa236@gmail.com>
2025-11-29 09:40:59 +0500

    matrix para

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-addEventListener('mousemove',function(e){
+let para =document.querySelector('p')
+let text=para.innerText
+const character="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz"
+let iteration=0
+let intervalId=null
 
- this.document.body.style.setProperty("--x",e.clientX+'px')
- 
- this.document.body.style.setProperty("--y",e.clientY+'px')
+
+para.addEventListener('mouseenter',function(){
+   
+clearInterval(intervalId)
+iteration=0
+ intervalId=setInterval(radomText, 30); 
 })
+
+function radomText(){
+let newtext=text.split("").map(function(char,index){
+    if(index<iteration){
+        return char
+    }
+        return character.split("")[Math.floor(Math.random()*character.length)]
+    }).join("")
+    para.innerText=newtext
+
+    iteration+=0.42
+    console.log('hry')
+
+    if(iteration>=text.length){
+        clearInterval(intervalId)
+         para.innerText=text
+    }
+}
